a1c450716888cad4578760ee2cd9cfe77ccdfde4
aclearworld <yamamichi@outlook.jp>
2018-06-23 14:55:08 +0900

    表示機能追加

--- SvgEditer/MainWindow.xaml.cs
+++ SvgEditer/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Svg;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -20,9 +23,28 @@ namespace SvgEditer
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 編集対象Svgファイル
+        /// </summary>
+        private SvgDocument svgDoc;
+
+
+        /// <summary>
+        /// Constructer
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            MessageBox.Show(Directory.GetCurrentDirectory().ToString());
+            this.svgDoc = Svg.SvgDocument.Open(@".\taarget.svg");
+
+            var bitmap = this.svgDoc.Draw();
+            this.image.Source = Imaging.CreateBitmapSourceFromHBitmap(bitmap.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions());
+
+
+
+
         }
     }
 }
